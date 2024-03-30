3bcab37ee01312f83ed88b17b3c1d0781a2acfdc
Ruslan <russam4515@gmail.com>
2024-03-30 18:45:27 -0400

    slugs

--- blog/views.py
+++ blog/views.py
@@ -72,9 +72,11 @@ def get_date(post):
 # Create your views here.
 
 def starting_page(request):
-  sorted_posts = sorted(all_posts, key=get_date)
-  latest_posts = sorted_posts[-3:]
-  return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+      "posts": latest_posts
+    })
 
 def posts(request): 
   return render(request, "blog/all-posts.html")
