71ad43628cdc2d809640a0e69bca989a91e3cfc5
Ruslan <russam4515@gmail.com>
2024-04-17 12:45:36 -0400

    first model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title =models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField()
