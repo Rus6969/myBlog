e2459da8a588ceeaa44413f56cb1771ecc87e0e4
Ruslan <russam4515@gmail.com>
2024-05-23 15:22:22 -0400

    css is added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from typing import Any
 from django.db.models.query import QuerySet
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView , DetailView , View
+from django.views.generic import ListView , View
 
 from .forms import CommentForm
 
@@ -64,20 +66,76 @@ class SinglePostView(View):
           is_saved_for_later = False
 
         return is_saved_for_later
-
+   # we need a slug to specify to choose specific post wee ned 
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
         
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            # here we have cretaed a form for single post view  and pass it in html 
             "comment_form": CommentForm(),
             "comments": post.comments.all().order_by("-id"),
             "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
-   
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            # do not create directly in db instead crete new model instance 
+            comment = comment_form.save(commit=False)
+            # build relations between post and comment 
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
+        }
+        return render(request, "blog/post-detail.html", context)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+          posts = Post.objects.filter(id__in=stored_posts)
+          context["posts"] = posts
+          context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+          stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+          stored_posts.append(post_id)
+        else:
+          stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
+        
+        return HttpResponseRedirect("/")
+
         
 
 
